587d081a1402228badff43bb3ba0b5f4a21b4bbe
shajmil-ufs <shajmil@ufstechnologies.com>
2024-06-21 18:36:33 +0530

    new setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chat-bot';
+  message: string | undefined;
+  messages: any[] = [];
+
+  constructor(private socket: Socket,) {   
+    
+   }
+   ngOnInit() { 
+    this.socket.fromEvent('new-email').subscribe((res :any)=> {
+        if (res && res.message) {
+            this.messages.push(res);
+        } else {
+            // If there's no message or the message is empty
+            this.messages.push({ message: "Sorry, I am not much aware of it", sender: "Server" });
+        }
+    });
+}
+
+
+
+  sendMessage() {
+    console.log('this.message: ', this.message); 
+    this.socket.emit('send-event', {message:this.message});
+    this.messages.push({sender:'you',message:this.message})
+    this.message = '';
+  }
+
+  Register() {
+
+    this.socket.emit('send-event', {isRegistration:true});
+    this.message = '';
+  }
+
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = {
+  url: 'http://localhost:3000',
+  options: {
+    transportOptions: {
+      polling: {
+        extraHeaders: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsiSWQiOjEsIlVzZXJuYW1lIjoiUmFqdSIsIkVtYWlsIjoicmFqdUBnbWFpbC5jb20iLCJQaG9uZV9OdW1iZXIiOiI0NTc4NCIsIlNvY2lhbF9JRCI6IiIsIlNvY2lhbF9Qcm92aWRlciI6IiJ9LCJpYXQiOjE3MTU1OTU3MjZ9.5Zi_LJlcviYkOsVfUkKztuM18jdc10eTpHVcUitR4Qg`, // Add the JWT token here
+        },
+      },
+    },
+  },
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
